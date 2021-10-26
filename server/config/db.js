14a33c57f39aee467cec79cec65cb5c73ca92464
iritsanna <iritsannamathew@gmail.com>
2021-10-27 00:45:41 +0530

    First Commit for MidTerm

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+   "URI":"mongodb+srv://irits:dtAmtipWrElA4jEW@mongodbserver.xnnlf.mongodb.net/books?retryWrites=true&w=majority",
+   "Secret":"Some Secret"
+  
+};
